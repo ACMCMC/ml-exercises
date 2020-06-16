44cb2c9c8c80ef56232731fead8651c1038955af
Aldan Creo <20495460+ACMCMC@users.noreply.github.com>
2020-06-17 00:41:56 +0200

    Ej1 - 1

---
+++ ex1/.ipynb_checkpoints/computeCost-checkpoint.m
@@ -0,0 +1,23 @@
+function J = computeCost(X, y, theta)
+%COMPUTECOST Compute cost for linear regression
+%   J = COMPUTECOST(X, y, theta) computes the cost of using theta as the
+%   parameter for linear regression to fit the data points in X and y
+
+% Initialize some useful values
+m = length(y); % number of training examples
+
+% You need to return the following variables correctly 
+J = 0;
+
+% ====================== YOUR CODE HERE ======================
+% Instructions: Compute the cost of a particular choice of theta
+%               You should set J to the cost.
+
+mat_hip = X*theta;
+mat_dif = mat_hip-y;
+J = sum(mat_dif.^2)/(2*rows(X));
+
+
+% =========================================================================
+
+end
